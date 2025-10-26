3c0dceeb285d34ae138fbb63dcf87e1759686866
Milan Hauth <milahu@gmail.com>
2025-10-26 19:42:31 +0100

    up shell.nix

--- shell.nix
+++ shell.nix
@@ -10,18 +10,43 @@ mkShell {
     gimp
     deskew
     tesseract
+    imagemagick
+    wget
+    pdftk
+    unzip
 
     # not used by tesseract?
     # hunspellDicts.de-de
     # hunspellDicts.en-us
 
-    # hocr editors
     # nur.repos.milahu.scribeocr
+
+    nur.repos.milahu.hocr-editor-qt
+
     # hocr-tools
-    # nur.repos.milahu.archive-hocr-tools
+
+    # hocr-to-epub-fxl
+    # https://github.com/internetarchive/archive-hocr-tools/pull/23
+    nur.repos.milahu.archive-hocr-tools
+
     # gImageReader
     # gImageReader-qt
 
     # prettier
+
+    (python3.withPackages (pp: with pp; [
+      pillow
+      numpy
+      opencv4
+      python-fontconfig
+      reportlab
+      ocrmypdf
+      psutil
+      pypdf2
+    ]))
+
+    img2pdf
+
+    # nur.repos.milahu.pdfjam
   ];
 }
